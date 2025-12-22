f19ea6b434e769027d4319d1c5528a840472eda4
Sai Khant Zay Lynn Yaung <khantzay.ly@gmail.com>
2025-12-23 00:34:35 +0700

    feat: Remove home.html and update API_BASE in config.js for local development; add QR code support in v-pdf.html

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Shared API config and helper
 (function (global) {
-  const API_BASE = 'https://dashboard.inkpactmm.org';
+  const API_BASE = 'http://localhost:3000';
 
   function toAbsolute(p) {
     if (!p) return p;
